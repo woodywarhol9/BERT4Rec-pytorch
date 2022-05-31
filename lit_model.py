7fee66bd7b6df249adab352310a44f38572279b9
woodywarhol9 <woodywarhol9@gmail.com>
2022-05-31 18:51:57 +0900

    Fix : indexes 변경. recs 처리에 torch.gather 사용 Feat : weight decay에서 bias, LayerNorm.weight 제외

--- lit_model.py
+++ lit_model.py
@@ -1,11 +1,9 @@
 import pytorch_lightning as pl
 import torch.nn as nn
-import torch.nn.functional as F
 import torch
 from torchmetrics import RetrievalHitRate, RetrievalNormalizedDCG
 
 from model import BERT
-
 
 class BERT4REC(pl.LightningModule):
     def __init__(self, args):
@@ -22,7 +20,8 @@ class BERT4REC(pl.LightningModule):
         self.head_num = args.head_num
         self.dropout_rate = args.dropout_rate
         self.dropout_rate_attn = args.dropout_rate_attn
-        self.vocab_size = args.vocab_size
+        self.vocab_size = args.item_size + 2
+        self.initializer_range = args.initializer_range
 
         self.neg_sample_size = args.neg_sample_size
         # Optimizer 관련 파라미터
@@ -38,14 +37,19 @@ class BERT4REC(pl.LightningModule):
             head_num=self.head_num,
             dropout_rate=self.dropout_rate,
             dropout_rate_attn=self.dropout_rate_attn,
+            initializer_range=self.initializer_range
         )
         # 토큰 예측
-        self.out = nn.Linear(self.hidden_dim, self.vocab_size)
+        # Mask의 예측이므로 args.item_size + 1
+        self.out = nn.Linear(self.hidden_dim, args.item_size + 1)
         # criterion 생성
         self.criterion = nn.CrossEntropyLoss(ignore_index=0)
         # HR@10, NDGC@10
+        # val, test 각각 선언.
+        self.batch_size = args.batch_size
+
         self.HR = RetrievalHitRate(k=10)
-        self.NDGC = RetrievalNormalizedDCG(k=10)
+        self.NDCG = RetrievalNormalizedDCG(k=10)
 
     def forward(self, x):
         """
@@ -72,9 +76,7 @@ class BERT4REC(pl.LightningModule):
         # pad는 예측에 제외되므로 ignore_index에 포함
         # labels : [batch_size, max_len]
         loss = self.criterion(preds.transpose(1, 2), labels)
-        self.log(
-            "train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True
-        )
+        self.log("train_loss", loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
 
         return loss
 
@@ -82,7 +84,7 @@ class BERT4REC(pl.LightningModule):
         """
         검증 : Mask Prediction 수행
         """
-        seq, candidates, _ = batch
+        seq, candidates, labels = batch
         logits = self.model(seq)
         preds = self.out(logits)
 
@@ -94,31 +96,23 @@ class BERT4REC(pl.LightningModule):
         loss = self.criterion(preds, targets)
         # neg_sample index 선택
         # recs : [batch_size, neg_sample + 1]
-        recs = torch.take(preds, candidates)
-        # labels [256, 101]
-        labels = candidates == targets.unsqueeze(1).expand(-1, self.neg_sample_size + 1)
+        recs = torch.gather(preds, 1, candidates)
         # HR, NDCG 구하기
-        indexes = torch.arange(labels.size(0), dtype=torch.long).unsqueeze(1).expand(-1, self.neg_sample_size + 1)
+        steps = batch_idx * self.batch_size
+        indexes = torch.arange(steps, steps + seq.size(0), dtype=torch.long).unsqueeze(1).repeat(1, 101)
         # HR@10, NDGC@10
         hr = self.HR(recs, labels, indexes)
-        ndcg = self.NDGC(recs, labels, indexes)
+        ndcg = self.NDCG(recs, labels, indexes)
         # 기록
-        self.log(
-            "val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-        self.log("HR_val", hr, on_step=False,
-                 on_epoch=True, prog_bar=True, logger=True)
-        self.log(
-            "NDCG_val", ndcg, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-
-        return loss
+        self.log("val_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("HR_val", hr, on_step = False, on_epoch=True, prog_bar = True, logger = True)
+        self.log("NDCG_val", ndcg, on_step = False, on_epoch=True, prog_bar = True, logger = True)
 
     def test_step(self, batch, batch_idx):
         """
         테스트 : Mask Prediction 수행
         """
-        seq, candidates, _ = batch
+        seq, candidates, labels = batch
         logits = self.model(seq)
         preds = self.out(logits)
 
@@ -130,40 +124,41 @@ class BERT4REC(pl.LightningModule):
         loss = self.criterion(preds, targets)
         # neg_sample index 선택
         # recs : [batch_size, neg_sample + 1]
-        recs = torch.take(preds, candidates)
-        # labels [256, 101]
-        labels = candidates == targets.unsqueeze(1).expand(-1, self.neg_sample_size + 1)
-        indexes = torch.arange(labels.size(0), dtype=torch.long).unsqueeze(1).expand(-1, self.neg_sample_size + 1)
+        recs = torch.gather(preds, 1, candidates)
+        # HR, NDCG 구하기
+        steps = batch_idx * self.batch_size
+        indexes = torch.arange(steps, steps + seq.size(0), dtype=torch.long).unsqueeze(1).repeat(1, 101)
         # HR, NDCG 구하기
         hr = self.HR(recs, labels, indexes)
-        ndcg = self.NDGC(recs, labels, indexes)
+        ndcg = self.NDCG(recs, labels, indexes)
         # 기록
-        self.log(
-            "test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-        self.log(
-            "HR_test", hr, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-        self.log(
-            "NDCG_test", ndcg, on_step=False, on_epoch=True, prog_bar=True, logger=True
-        )
-
-        return loss
+        self.log("test_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
+        self.log("HR_test", hr, on_step = False, on_epoch =True, prog_bar = True, logger = True)
+        self.log("NDCG_test", ndcg, on_step = False, on_epoch =True, prog_bar = True, logger = True)
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(
-            self.parameters(), lr=self.learning_rate, weight_decay=self.weight_decay
-        )
+        """
+        Optimizer, Scheduler 초기화.
+        """
+        no_decay = ['bias', 'LayerNorm.weight']
+        #No decay bias, LayerNorm.weight
+        optimizer_grouped_parameters = [
+            {'params': [p for n, p in self.named_parameters() if not any(nd in n for nd in no_decay)],
+            'weight_decay': self.weight_decay},
+            {'params': [p for n, p in self.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+                                        ]
+        optimizer = torch.optim.Adam(optimizer_grouped_parameters, lr=self.learning_rate)
         scheduler = torch.optim.lr_scheduler.StepLR(
             optimizer, step_size=self.decay_step, gamma=self.gamma
         )
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "HR_val"}
+        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "val_loss"}
 
     # 인스턴스 없이도 확인할 수 있도록 static method로 설정
-
     @staticmethod
     def add_to_argparse(parser):
-        # 학습 관련
+        """
+        Training 관련 args 입력.
+        """
         parser.add_argument("--learning_rate", type=float, default=1e-3)
         # BERT 모델 관련
         parser.add_argument("--hidden_dim", type=int, default=256)
@@ -171,9 +166,10 @@ class BERT4REC(pl.LightningModule):
         parser.add_argument("--head_num", type=int, default=4)
         parser.add_argument("--dropout_rate", type=float, default=0.1)
         parser.add_argument("--dropout_rate_attn", type=float, default=0.1)
+        parser.add_argument("--initializer_range", type=float, default=0.02)
         # Optimizer 관련
         parser.add_argument("--weight_decay", type=float, default=0.01)
         parser.add_argument("--decay_step", type=int, default=25)
-        parser.add_argument("--gamma", type=float, default=1.0)
+        parser.add_argument("--gamma", type=float, default=0.1)
 
         return parser
