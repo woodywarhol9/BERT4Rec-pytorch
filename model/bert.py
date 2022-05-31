769e100b315aefb4e73a2356c1b0fcd8cc4eca0c
woodywarhol9 <woodywarhol9@gmail.com>
2022-05-31 19:10:15 +0900

    Fix : SublayerConnection 변경

--- model/bert.py
+++ model/bert.py
@@ -21,22 +21,19 @@ class BERTEmbeddings(nn.Module):
         """
         super(BERTEmbeddings, self).__init__()
         # [0] Token은 padding에 사용되기 때문에 Embedding 계산하지 않음.
-        self.token_embeddings = nn.Embedding(
-            vocab_size, embed_size, padding_idx=0)
+        self.token_embeddings = nn.Embedding(vocab_size, embed_size, padding_idx=0)
         self.position_embeddings = nn.Embedding(max_len, embed_size)
         # 어느 문장에 속하는 지 Token 정보 저장. 이때, [0] 토큰은 무시 됨.
         self.segment_embeddings = nn.Embedding(3, embed_size, padding_idx=0)
         # layer_norm + dropout
         self.layer_norm = nn.LayerNorm(embed_size, eps=1e-6)
         self.dropout = nn.Dropout(p=dropout_rate)
-        # self.embed_size = embed_size -> 안 쓰일듯
 
     def forward(self, seq, segment_label=None):
         # seq : (batch, seq_len)
         batch_size, seq_length = seq.size(0), seq.size(1)
         # position-ids : idx로 활용되기 때문에 torch.long으로 생성
-        position_ids = torch.arange(
-            seq.size(1), dtype=torch.long, device=seq.device)
+        position_ids = torch.arange(seq_length, dtype=torch.long, device=seq.device)
         # position_ids를 (batch_size, seq_length) 형태로 변경.
         position_ids = position_ids.unsqueeze(0).expand(batch_size, seq_length)
         # token , position embeddings, segment embeddings
@@ -49,8 +46,7 @@ class BERTEmbeddings(nn.Module):
             segment_embeddings = self.segment_embeddings(segment_label)
             embeddings += segment_embeddings
         # layer-norm + drop out
-        embeddings = self.layer_norm(embeddings)
-        embeddings = self.dropout(embeddings)
+        embeddings = self.dropout(self.layer_norm(embeddings))
 
         return embeddings
 
@@ -92,14 +88,9 @@ class MultiHeadedAttention(nn.Module):
         value = self.value_linear(v)
         # head로 분리
         # [batch, len, head 수, head 차원] -> [batch, head_num, len, head_dim]
-        query = query.view(batch_size, -1, self.head_num, self.head_dim).permute(
-            0, 2, 1, 3
-        )
-        key = key.view(batch_size, -1, self.head_num,
-                       self.head_dim).permute(0, 2, 1, 3)
-        value = value.view(batch_size, -1, self.head_num, self.head_dim).permute(
-            0, 2, 1, 3
-        )
+        query = query.view(batch_size, -1, self.head_num, self.head_dim).permute(0, 2, 1, 3)
+        key = key.view(batch_size, -1, self.head_num, self.head_dim).permute(0, 2, 1, 3)
+        value = value.view(batch_size, -1, self.head_num, self.head_dim).permute(0, 2, 1, 3)
         # attention 스코어 계산
         # scores : [batch, head_num, query_len, key_len]
         scores = torch.matmul(query, key.permute(0, 1, 3, 2)) / self.scale
@@ -112,10 +103,10 @@ class MultiHeadedAttention(nn.Module):
         attention = F.softmax(scores, dim=-1)
         attention = self.dropout(attention)
         # attention과 value로 output 계산
-        # output : [batch, head_num, query_len, head_dim]
+        # attention_seq : [batch, head_num, query_len, head_dim]
         # view를 실행하기 위해서 contiguous()
         attention_seq = torch.matmul(attention, value).contiguous()
-        # output : [batch, query_len, hidden_dim]
+        # attention_seq : [batch, query_len, hidden_dim]
         attention_seq = attention_seq.view(batch_size, -1, self.hidden_dim)
         attention_seq = self.output_linear(attention_seq)
 
@@ -130,12 +121,13 @@ class SublayerConnection(nn.Module):
 
     def __init__(self, hidden_dim, dropout_rate=0.1):
         super(SublayerConnection, self).__init__()
+        
         self.layer_norm = nn.LayerNorm(hidden_dim, eps=1e-6)
         self.dropout = nn.Dropout(p=dropout_rate)
 
     def forward(self, layer, sublayer):
         "Apply residual connection to any sublayer with the same size."
-        return self.layer_norm(layer + self.dropout(sublayer))
+        return layer + self.dropout(self.layer_norm(sublayer))
 
 
 class PositionwiseFeedForward(nn.Module):
@@ -162,7 +154,7 @@ class TransformerEncoder(nn.Module):
     """
 
     def __init__(
-        self, hidden_dim, head_num, ff_dim, dropout_rate=0.1, dropout_rate_attn=0.1
+        self, hidden_dim, head_num, ff_dim, dropout_rate = 0.1, dropout_rate_attn = 0.1
     ):
         """
         :param hidden_dim: hidden dim of transformer
@@ -198,6 +190,7 @@ class TransformerEncoder(nn.Module):
         self.output_sublayer = SublayerConnection(
             hidden_dim=self.hidden_dim, dropout_rate=self.dropout_rate
         )
+        self.dropout = nn.Dropout(p=dropout_rate)
 
     def forward(self, seq, mask):
         # attention 결과
@@ -206,8 +199,9 @@ class TransformerEncoder(nn.Module):
         # sublayer connection 1 진행 : seq embeddings과 attention 결합
         connected_layer = self.input_sublayer(seq, attention_seq)
         # sublayer connection 2 진행 : conncted_layer와 FFN을 통과한 connected_layer 결합
-        return self.output_sublayer(connected_layer, self.feed_forward(connected_layer))
+        connected_layer = self.output_sublayer(connected_layer, self.feed_forward(connected_layer))
 
+        return self.dropout(connected_layer)
 
 class BERT(nn.Module):
     """
@@ -223,6 +217,7 @@ class BERT(nn.Module):
         head_num=12,
         dropout_rate=0.1,
         dropout_rate_attn=0.1,
+        initializer_range=0.02
     ):
         """
         :param vocab_size: vocab_size of total words
@@ -242,16 +237,13 @@ class BERT(nn.Module):
         self.layer_num = layer_num
         # head_num
         self.head_num = head_num
-        self.head_dim = hidden_dim / head_num
         # dropout 비율
         self.dropout_rate = dropout_rate
         self.dropout_rate_attn = dropout_rate_attn
         # paper noted they used 4*hidden_size for ff_network_hidden_size
         self.ff_dim = hidden_dim * 4
         # embedding
-        self.embedding = BERTEmbeddings(
-            vocab_size=self.vocab_size, embed_size=self.hidden_dim, max_len=self.max_len
-        )
+        self.embedding = BERTEmbeddings(vocab_size=self.vocab_size, embed_size=self.hidden_dim, max_len=self.max_len)
         # Transformer Encoder
         self.transformer_encoders = nn.ModuleList(
             [
@@ -265,6 +257,23 @@ class BERT(nn.Module):
                 for _ in range(self.layer_num)
             ]
         )
+        # weight initialization
+        self.initializer_range = initializer_range
+        self.apply(self._init_weights)
+
+    def _init_weights(self, module):
+        """ Initialize the weights.
+        """
+        if isinstance(module, (nn.Linear, nn.Embedding)):
+            # Slightly different from the TF version which uses truncated_normal for initialization
+            # cf https://github.com/pytorch/pytorch/pull/5617
+            module.weight.data.normal_(mean=0.0, std=self.initializer_range)
+        elif isinstance(module, nn.LayerNorm):
+            module.bias.data.zero_()
+            module.weight.data.fill_(1.0)
+        if isinstance(module, nn.Linear) and module.bias is not None:
+            module.bias.data.zero_()
+
 
     def forward(self, seq, segment_info=None):
         # attention masking for padded token
@@ -277,5 +286,5 @@ class BERT(nn.Module):
         # running over multiple transformer blocks
         for transformer in self.transformer_encoders:
             seq = transformer(seq, mask)
-
+        
         return seq
