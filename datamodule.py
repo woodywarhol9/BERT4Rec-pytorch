aeff4ecb98744631eb8df0bc1851b095d622a034
woodywarhol9 <woodywarhol9@gmail.com>
2022-06-03 17:59:30 +0900

    Refactor : Docstring 추가, comment 수정, type hint 추가

--- datamodule.py
+++ datamodule.py
@@ -1,53 +1,51 @@
 import pytorch_lightning as pl
-from torch.utils.data import DataLoader
+
 from data import MovieLens
 
-MOVIE_LENS = "data/ml-1m/ratings.dat"
+from torch.utils.data import DataLoader
+from typing import Optional
 
-# DataModule 설정
+MOVIE_LENS = "data/ml-1m/ratings.dat"
 
 
 class DataModule(pl.LightningDataModule):
     """
-    DataModule : Dataset을 받아와 Dataloader로 반환.
+    DataModule
+    - train/valid/test/predict dataloader 생성
     """
-
     def __init__(self, args):
+        """
+        DataModule 초기화
+        - Dataset args
+        """
         super(DataModule, self).__init__()
-        # seq 최대 길이
+        # Dataset 관련 설정
         self.max_len = args.max_len
-        # masking 기준 확률
         self.mask_prob = args.mask_prob
-        # neg sample 수
         self.neg_sample_size = args.neg_sample_size
-        # 데이터 경로
         self.data_dir = args.data_dir
-        # batch_size 설정
-        self.batch_size = args.batch_size
         # DataLoader 관련 설정
         self.pin_memory = args.pin_memory
         self.num_workers = args.num_workers
-        # 훈련 데이터
+        self.batch_size = args.batch_size
+        # vocab size 할당
         self.movie_train = MovieLens(
             mode="Train", max_len=self.max_len, mask_prob=self.mask_prob, data_dir=self.data_dir)
-        # vocab size 재할당
         args.item_size = self.movie_train.item_size
 
     def prepare_data(self):
         """
-        데이터 다운로드 진행. 이미 있으니까 pass.
+        Data 다운로드 진행
         """
         pass
 
-    def setup(self, stage=None):
+    def setup(self, stage: Optional[str] = None) -> None:
         """
-        Train/Valid/Test 데이터셋 생성.
+        Train/Valid/Test 데이터셋 생성
         """
         # Train/Valid 데이터셋 생성
+        # args.item size 할당을 위해 train dataset은 __init__에서 정의
         if stage == "fit" or stage is None:
-            # train data
-            # vocab_size 얻기 위해서 미리 선언.
-            # valid data
             self.movie_valid = MovieLens(mode="Valid", max_len=self.max_len, mask_prob=self.mask_prob, data_dir=self.data_dir,
                                          neg_sample_size=self.neg_sample_size)
         # Test 데이터셋 생성
@@ -55,26 +53,33 @@ class DataModule(pl.LightningDataModule):
             self.movie_test = MovieLens(mode="Test", max_len=self.max_len, mask_prob=self.mask_prob, data_dir=self.data_dir,
                                         neg_sample_size=self.neg_sample_size)
 
-    def train_dataloader(self):
+    def train_dataloader(self) -> DataLoader:
         return DataLoader(self.movie_train, batch_size=self.batch_size, shuffle=True, pin_memory=self.pin_memory, num_workers=self.num_workers)
 
-    def val_dataloader(self):
+    def val_dataloader(self) -> DataLoader:
         return DataLoader(self.movie_valid, batch_size=self.batch_size, shuffle=False, pin_memory=self.pin_memory, num_workers=self.num_workers)
 
-    def test_dataloader(self):
+    def test_dataloader(self) -> DataLoader:
         return DataLoader(self.movie_test, batch_size=self.batch_size, shuffle=False, pin_memory=self.pin_memory, num_workers=self.num_workers)
-    # 인스턴스 없이도 확인할 수 있도록 static method로 설정
+
+    def predict_dataloader(self) -> DataLoader:
+        pass
+
     @staticmethod
     def add_to_argparse(parser):
+        """
+        DataModule 관련 args 정의
+        """
+        # Dataset 관련
         parser.add_argument('--max_len', type=int, default=100)
         parser.add_argument('--mask_prob', type=float, default=0.2)
         parser.add_argument('--neg_sample_size', type=int, default=100)
-        parser.add_argument('--data_dir', type=str, default=MOVIE_LENS)
         # DataLoader 관련
         parser.add_argument('--batch_size', type=int, default=256)
         parser.add_argument('--pin_memory', type=bool, default=True)
         parser.add_argument('--num_workers', type=int, default=4)
         # ml - 1m
         parser.add_argument('--item_size', type=int, default=3706)
-        
+        parser.add_argument('--data_dir', type=str, default=MOVIE_LENS)
+
         return parser
